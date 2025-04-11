2f2b1a4876436451a081c859dc48b0d05663a08c
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-04-11 16:45:22 +0700

    isPalindrome number 2

--- src/Palindrome Number/palindromeNumber.ts
+++ src/Palindrome Number/palindromeNumber.ts
@@ -6,3 +6,16 @@ export function isPalindrome(x: number): boolean {
 }
 
 // Palindrome number : là kiểm tra xem số đó có phải số đối xứng không
+
+export function isPalindrome2(x: number): boolean {
+  // kiểm tra xem coi có trường hợp số âm , hoặc nếu số mà khác 0 chia hết cho 10 thì không phải là số đối xứng như 100 , 10
+  if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+
+  // nếu x > reversed thì lấy phần tử cuối bỏ vào reversed
+  let reversed = 0;
+  while (x > reversed) {
+    reversed = reversed * 10 + (x % 10); // lấy phần tử cuối của x , reversed * 10 dịch chuyển thêm 1 đơn vị
+    x = Math.floor(x / 10); // xóa số cuối cùng dùng thêm math.floor để làm tròn
+  }
+  return x === reversed || x === Math.floor(reversed / 10); // ví dụ 12 == 12 nếu  như 12 với 123 thì Math.floor(reversed / 10) làm tròn
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { twoSum } from "./TwoSum/twoSum";
-import { isPalindrome } from "./Palindrome Number/palindromeNumber";
+import {
+  isPalindrome,
+  isPalindrome2,
+} from "./Palindrome Number/palindromeNumber";
 // const nums = [2, 7, 11, 15];
 // const target = 9;
 // console.log(twoSum(nums, target));
 
-console.log(isPalindrome(121));
+console.log(isPalindrome2(12321));
